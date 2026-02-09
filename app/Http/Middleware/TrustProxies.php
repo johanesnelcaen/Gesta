a115a2dd2fa89c28e7c0ab04bff67b32eb0b2388
Johanes ZINSOUWI <johanesnelcaen@gmail.com>
2026-02-09 18:21:52 +0100

    Force HTTPS for assets (Railway)

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Http\Middleware\TrustProxies as Middleware;
 use Illuminate\Http\Request;
+use Fideloper\Proxy\TrustProxies;
 
-class TrustProxies extends Middleware
-{
-    protected $proxies = '*';
+$middleware->trustProxies(
+    '*', // ou ['*']
+    Request::HEADER_X_FORWARDED_FOR | 
+    Request::HEADER_X_FORWARDED_HOST |
+    Request::HEADER_X_FORWARDED_PROTO |
+    Request::HEADER_X_FORWARDED_PORT
+);
 
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
-}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,11 @@ return Application::configure(basePath: dirname(__DIR__))
    ->withMiddleware(function ($middleware) {
     $middleware->trustProxies(
         at: '*',
-        headers: \Illuminate\Http\Request::HEADER_X_FORWARDED_ALL
+       headers: \Illuminate\Http\Request::HEADER_X_FORWARDED_FOR |
+         \Illuminate\Http\Request::HEADER_X_FORWARDED_HOST |
+         \Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO |
+         \Illuminate\Http\Request::HEADER_X_FORWARDED_PORT
+
     );
 })
     ->withExceptions(function (Exceptions $exceptions) {
